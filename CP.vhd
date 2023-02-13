ef72bcf4fbbdae6b1d4580f58454f4972526f795
Richi <tremon36xd@gmail.com>
2023-02-13 17:39:37 +0100

    revert bug

--- CP.vhd
+++ CP.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
@@ -9,7 +8,7 @@ entity CP is
         current_num:out std_logic_vector(31 downto 0);
         load: in std_logic_vector(31 downto 0)
         );
-      
+
 end CP;
 
 architecture Behavioral of CP is
@@ -17,12 +16,11 @@ signal num1: std_logic_vector(31 downto 0);
 
 begin
 current_num <= num1;
-    process (clock,enable_parallel_load)
+    process (clock)
     begin
-        if(enable_parallel_load = '1') then num1 <= load;
-        else
         if (clock = '1' and clock'event)then 
             if(reset = '1') then num1 <= X"00000000";
+            else if(enable_parallel_load = '1') then num1 <= load;
             else if(count = '1' ) then num1 <= num1 + "100";
                  end if;
                  end if;
